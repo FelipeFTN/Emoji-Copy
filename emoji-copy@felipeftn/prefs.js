764a593a91cd6f04f06e0bc2bc85a77d2f2b1621
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-09-01 18:27:19 -0300

    🦜 global settings

--- emoji-copy@felipeftn/prefs.js
+++ emoji-copy@felipeftn/prefs.js
@@ -7,22 +7,22 @@ const _ = Gettext.gettext;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-let SETTINGS;
 
 //------------------------------------------------------------------------------
 
 function init() {
 	ExtensionUtils.initTranslations();
-    SETTINGS = ExtensionUtils.getSettings();
 }
 
 //------------------------------------------------------------------------------
 
-const EmojiSelectorSettingsWidget = new GObject.Class({
-	Name: 'EmojiSelector.Prefs.Widget',
-	GTypeName: 'EmojiSelectorPrefsWidget',
+const EmojiCopySettingsWidget = new GObject.Class({
+	Name: 'EmojiCopy.Prefs.Widget',
+	GTypeName: 'EmojiCopyPrefsWidget',
 
-	_init (params) {
+	_init (settings) {
+        this.settings = settings;
+
 		let builder = new Gtk.Builder();
 		builder.add_from_file(Me.path+'/prefs.ui');
 		this.prefs_stack = builder.get_object('prefs_stack');
@@ -39,41 +39,41 @@ const EmojiSelectorSettingsWidget = new GObject.Class({
 		let positionCombobox = builder.get_object('position_combobox');
 		positionCombobox.append('top', _("From top to bottom"));
 		positionCombobox.append('bottom', _("From bottom to top"));
-		positionCombobox.active_id = SETTINGS.get_string('position');
+		positionCombobox.active_id = this.settings.get_string('position');
 
 		positionCombobox.connect("changed", (widget) => {
-			SETTINGS.set_string('position', widget.get_active_id());
+			this.settings.set_string('position', widget.get_active_id());
 		});
 
-		SETTINGS.connect('changed::position', () => {
-			positionCombobox.set_active_id(SETTINGS.get_string('position'));
+		this.settings.connect('changed::position', () => {
+			positionCombobox.set_active_id(this.settings.get_string('position'));
 		});
 
 		//----------------------------------------------------------------------
 
 		let emojiSize = builder.get_object('size_spinbtn');
-		emojiSize.set_value(SETTINGS.get_int('emojisize'));
+		emojiSize.set_value(this.settings.get_int('emojisize'));
 
 		emojiSize.connect('value-changed', w => {
 			var value = w.get_value_as_int();
-			SETTINGS.set_int('emojisize', value);
+			this.settings.set_int('emojisize', value);
 		});
 
-		SETTINGS.connect('changed::emojisize', () => {
-			emojiSize.set_value(SETTINGS.get_int('emojisize'));
+		this.settings.connect('changed::emojisize', () => {
+			emojiSize.set_value(this.settings.get_int('emojisize'));
 		});
 
 		//----------------------------------------------------------------------
 
 		let nbColsSpinBtn = builder.get_object('nbcols_spinbtn');
-		nbColsSpinBtn.set_value(SETTINGS.get_int('nbcols'));
+		nbColsSpinBtn.set_value(this.settings.get_int('nbcols'));
 		nbColsSpinBtn.connect('value-changed', w => {
 			var value = w.get_value_as_int();
-			SETTINGS.set_int('nbcols', value);
+			this.settings.set_int('nbcols', value);
 		});
 
-		SETTINGS.connect('changed::nbcols', () => {
-			nbColsSpinBtn.set_value(SETTINGS.get_int('nbcols'));
+		this.settings.connect('changed::nbcols', () => {
+			nbColsSpinBtn.set_value(this.settings.get_int('nbcols'));
 		});
 
 		//----------------------------------------------------------------------
@@ -84,63 +84,63 @@ const EmojiSelectorSettingsWidget = new GObject.Class({
 		builder.get_object('default-kbs-help-2').set_label(RELOAD_TEXT);
 
 		let keybindingEntry = builder.get_object('keybinding_entry');
-		keybindingEntry.set_sensitive(SETTINGS.get_boolean('use-keybinding'));
+		keybindingEntry.set_sensitive(this.settings.get_boolean('use-keybinding'));
 
-		if (SETTINGS.get_strv('emoji-keybinding') != '') {
-			keybindingEntry.text = SETTINGS.get_strv('emoji-keybinding')[0];
+		if (this.settings.get_strv('emoji-keybinding') != '') {
+			keybindingEntry.text = this.settings.get_strv('emoji-keybinding')[0];
 		}
 
-		SETTINGS.connect('changed::emoji-keybinding', () => {
-			keybindingEntry.set_text(SETTINGS.get_strv('emoji-keybinding')[0]);
+		this.settings.connect('changed::emoji-keybinding', () => {
+			keybindingEntry.set_text(this.settings.get_strv('emoji-keybinding')[0]);
 		});
 
 		let keybindingButton = builder.get_object('keybinding_button');
-		keybindingButton.set_sensitive(SETTINGS.get_boolean('use-keybinding'));
+		keybindingButton.set_sensitive(this.settings.get_boolean('use-keybinding'));
 
 		keybindingButton.connect('clicked', widget => {
-			SETTINGS.set_strv('emoji-keybinding', [keybindingEntry.text]);
+			this.settings.set_strv('emoji-keybinding', [keybindingEntry.text]);
 		});
 
 		//----------------------------------------------------------------------
 
 		let keybindingSwitch = builder.get_object('keybinding_switch');
-		keybindingSwitch.set_state(SETTINGS.get_boolean('use-keybinding'));
+		keybindingSwitch.set_state(this.settings.get_boolean('use-keybinding'));
 
 		keybindingSwitch.connect('notify::active', widget => {
 			if (widget.active) {
-				SETTINGS.set_boolean('use-keybinding', true);
+				this.settings.set_boolean('use-keybinding', true);
 				keybindingEntry.sensitive = true;
 				keybindingButton.sensitive = true;
 				alwaysShowSwitch.sensitive = true;
 			} else {
-				SETTINGS.set_boolean('use-keybinding', false);
+				this.settings.set_boolean('use-keybinding', false);
 				keybindingEntry.sensitive = false;
 				keybindingButton.sensitive = false;
-				SETTINGS.set_boolean('always-show', true);
+				this.settings.set_boolean('always-show', true);
 				alwaysShowSwitch.set_state(true);
 				alwaysShowSwitch.sensitive = false;
 			}
 		});
 
-		SETTINGS.connect('changed::use-keybinding', () => {
-			keybindingSwitch.set_state(SETTINGS.get_boolean('use-keybinding'));
+		this.settings.connect('changed::use-keybinding', () => {
+			keybindingSwitch.set_state(this.settings.get_boolean('use-keybinding'));
 		});
 
 		//----------------------------------------------------------------------
 
 		let alwaysShowSwitch = builder.get_object('always_show_switch');
-		alwaysShowSwitch.set_state(SETTINGS.get_boolean('always-show'));
+		alwaysShowSwitch.set_state(this.settings.get_boolean('always-show'));
 
 		alwaysShowSwitch.connect('notify::active', widget => {
 			if (widget.active) {
-				SETTINGS.set_boolean('always-show', true);
+				this.settings.set_boolean('always-show', true);
 			} else {
-				SETTINGS.set_boolean('always-show', false);
+				this.settings.set_boolean('always-show', false);
 			}
 		});
 
-		SETTINGS.connect('changed::always-show', () => {
-			alwaysShowSwitch.set_state(SETTINGS.get_boolean('always-show'));
+		this.settings.connect('changed::always-show', () => {
+			alwaysShowSwitch.set_state(this.settings.get_boolean('always-show'));
 		});
 	},
 
@@ -164,7 +164,7 @@ const EmojiSelectorSettingsWidget = new GObject.Class({
 //------------------------------------------------------------------------------
 
 function buildPrefsWidget() {
-	let widget = new EmojiSelectorSettingsWidget();
+	let widget = new EmojiCopySettingsWidget(ExtensionUtils.getSettings());
 	let obj = widget.prefs_stack;
 
 	obj.connect('realize', () => {
