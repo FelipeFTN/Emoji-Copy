9863f1e9db0bbcb58a34b724a0e3671ff059960c
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-12-03 14:25:41 -0300

    😁 emoji keybind

--- emoji-copy@felipeftn/prefs.js
+++ emoji-copy@felipeftn/prefs.js
@@ -5,13 +5,23 @@ import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Ex
 
 export default class EmojiCopyPrefs extends ExtensionPreferences {
     fillPreferencesWindow(window) {
+        // Constructor
         window._settings = this.getSettings();
+        this._window = window;
 
+        // General Page
+        this._general();
+
+        // About Page
+        this._about();
+    }
+
+    _general() {
         const general_pg = new Adw.PreferencesPage({
             title: _('General'),
             icon_name: 'emoji-symbols-symbolic',
         });
-        window.add(general_pg);
+        this._window.add(general_pg);
 
         const general_gp = new Adw.PreferencesGroup({
             title: _('General Settings'),
@@ -31,8 +41,7 @@ export default class EmojiCopyPrefs extends ExtensionPreferences {
         });
         general_gp.add(active_keybind);
 
-        const keybind = window._settings.get_strv('emoji-keybind')[0];
-        // console.log(`\n KEYBIND: ${keybind}; TYPE: ${typeof(keybind[0])}; STRINGIFY: ${JSON.stringify(keybind)}\n`);
+        let keybind = this._window._settings.get_strv('emoji-keybind')[0];
         const emoji_keybind = new Adw.EntryRow({
             title: _('Emoji Copy Keybind'),
             show_apply_button:  true,
@@ -40,23 +49,28 @@ export default class EmojiCopyPrefs extends ExtensionPreferences {
         });
         general_gp.add(emoji_keybind);
 
+        emoji_keybind.connect('changed', () => {
+            keybind = emoji_keybind.get_text();
+		});
+        emoji_keybind.connect('apply', () => {
+			this._window._settings.set_strv('emoji-keybind', [keybind]);
+		});
+
+        this._window._settings.bind('always-show', show_indicator, 'active', Gio.SettingsBindFlags.DEFAULT);
+        this._window._settings.bind('active-keybind', active_keybind, 'active', Gio.SettingsBindFlags.DEFAULT);
+    }
+
+    _about() {
         const about_pg = new Adw.PreferencesPage({
             title: _('About'),
             icon_name: 'dialog-information-symbolic',
         });
-        window.add(about_pg);
+        this._window.add(about_pg);
 
         const about_gp = new Adw.PreferencesGroup({
             title: _('About'),
             description: _('Emoji Copy'),
         });
         about_pg.add(about_gp);
-
-        window._settings.bind('always-show', show_indicator, 'active', Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('active-keybind', active_keybind, 'active', Gio.SettingsBindFlags.DEFAULT);
-        // Working in Progress
-        // emoji_keybind.set_text(emoji_keybind.text);
-        // window._settings.set_strv('emoji-keybind', [emoji_keybind.text]);
-        // window._settings.bind('emoji-keybind', emoji_keybind, 'value', Gio.SettingsBindFlags.DEFAULT);
     }
 }
