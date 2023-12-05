d1d84765e2561c65466d4f6d088ebe355a0ad827
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-12-05 00:39:18 -0300

    ⚠️ about emoji-copy

--- emoji-copy@felipeftn/prefs.js
+++ emoji-copy@felipeftn/prefs.js
@@ -1,5 +1,6 @@
 import Gio from 'gi://Gio';
 import Adw from 'gi://Adw';
+import Gtk from 'gi://Gtk';
 
 import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
@@ -9,11 +10,12 @@ export default class EmojiCopyPrefs extends ExtensionPreferences {
         window._settings = this.getSettings();
         this._window = window;
 
+        const iconTheme = Gtk.IconTheme.get_for_display(window.get_display());
+        const iconsDirectory = this.dir.get_child('icons').get_path();
+        iconTheme.add_search_path(iconsDirectory);
+
         // General Page
         this._general();
-
-        // About Page
-        this._about();
     }
 
     _general() {
@@ -62,31 +64,40 @@ export default class EmojiCopyPrefs extends ExtensionPreferences {
 			this._window._settings.set_strv('emoji-keybind', [keybind]);
 		});
 
+        // About button - Thanks to @maniacx this could be done perfectly! 
+        const about_button = new Gtk.Button({
+            margin_top : 8,
+            margin_bottom : 8,
+            child: new Adw.ButtonContent({
+                icon_name: 'dialog-information-symbolic',
+                label: _('About')
+            })
+        });
+
+        // About Button Styles
+        about_button.set_css_classes(['accent']);
+        // about_button.set_css_classes(['suggested-action']);
+        // about_button.set_css_classes(['raised']);
+
+        general_gp.set_header_suffix(about_button);
+
+        about_button.connect('clicked', () => { this._openAboutPage(); });
+
         // Bind Adwaita field values to schema
         this._window._settings.bind('always-show', show_indicator, 'active', Gio.SettingsBindFlags.DEFAULT);
         this._window._settings.bind('active-keybind', active_keybind, 'active', Gio.SettingsBindFlags.DEFAULT);
     }
 
-    _about() {
-        // About page
-        const about_pg = new Adw.PreferencesPage({
-            title: _('About'),
-            icon_name: 'dialog-information-symbolic',
-        });
-        this._window.add(about_pg);
-
-        // Group about page
-        const about_gp = new Adw.PreferencesGroup({
-            title: _('About'),
-            description: _('Emoji Copy'),
-        });
-        about_pg.add(about_gp);
-
-        const about_window = new Adw.AboutWindow({
-            application_name: _('Emoji Copy'),
-            developer_name: _('FelipeFTN & Community'),
-            version: _('14'),
-        });
-        about_gp.add(about_window);
+    _openAboutPage() {
+        const about_window = new Adw.AboutWindow({transient_for: this._window, modal: true});
+        about_window.set_application_icon('emoji-people-symbolic');
+        about_window.set_application_name(_('Emoji Copy'));
+        about_window.set_version(`${this.metadata.version}.0`);
+        about_window.set_developer_name('FelipeFTN & Community');
+        about_window.set_issue_url('https://github.com/FelipeFTN/Emoji-Copy/issues');
+        about_window.set_website('https://github.com/FelipeFTN/Emoji-Copy');
+        about_window.set_license_type(Gtk.License.GPL_3_0);
+        about_window.set_copyright('© 2023 FelipeFTN');
+        about_window.show();
     }
 }
