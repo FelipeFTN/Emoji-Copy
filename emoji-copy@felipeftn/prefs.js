de8d41bec3b83996f3987e849327ae9aed5acf59
FelipeFTN <FelipeFTN@protonmail.com>
2026-07-28 10:22:14 -0300

    feat: adding shortcuts in the preferences UI

--- emoji-copy@felipeftn/prefs.js
+++ emoji-copy@felipeftn/prefs.js
@@ -18,6 +18,27 @@ export default class EmojiCopyPrefs extends ExtensionPreferences {
         this._general();
     }
 
+    _shortcuts(page) {
+        const shortcuts_gp = new Adw.PreferencesGroup({
+            title: _('Shortcuts'),
+            description: _('Ways to pick an emoji from the menu'),
+        });
+        page.add(shortcuts_gp);
+
+        const shortcuts = [
+            [_('Left-click / Enter'), _('Copy the emoji and close the menu')],
+            [_('Middle-click / Ctrl+Enter'), _('Copy the emoji without closing the menu')],
+            [_('Right-click / Shift+Enter'), _('Add the emoji to the end of the clipboard content')],
+            [_('Tab'), _('Navigate between emojis')],
+        ];
+        for (const [keys, description] of shortcuts) {
+            shortcuts_gp.add(new Adw.ActionRow({
+                title: keys,
+                subtitle: description,
+            }));
+        }
+    }
+
     _general() {
         // General page
         const general_pg = new Adw.PreferencesPage({
@@ -125,6 +146,9 @@ export default class EmojiCopyPrefs extends ExtensionPreferences {
         this._window._settings.bind('paste-on-select', paste_on_select, 'active', Gio.SettingsBindFlags.DEFAULT);
         this._window._settings.bind('keep-open', keep_open, 'active', Gio.SettingsBindFlags.DEFAULT);
         this._window._settings.bind('active-keybind', active_keybind, 'active', Gio.SettingsBindFlags.DEFAULT);
+
+        // Mouse and keyboard shortcuts reference
+        this._shortcuts(general_pg);
     }
 
     _openAboutPage() {
